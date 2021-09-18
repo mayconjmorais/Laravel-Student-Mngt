cfb2cb1a60c1a49c5fa1a246f6de3fcc1b8c6a88
mayconjmorais <mayconjmorais@gmail.com>
2021-09-18 16:59:35 -0400

    Menubar implemented in the Index view

---
+++ app/includes/menubar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">RESUME</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">SKILLS </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">PORTFOLIO </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">EXPERIENCE </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT </a>
+      </li> 
+    </ul>
+  </div>
+</nav>
